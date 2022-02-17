dd52833f7614febc65857562e1db3d111f98b865
Sergey Kadiyevskiy <51814805+Kalashnik0v1947@users.noreply.github.com>
2022-02-17 16:06:04 -0500

    updated again

--- config/session.config.js
+++ config/session.config.js
@@ -1,25 +1,27 @@
-const session = require('express-session');
+const session = require("express-session");
 
-const MongoStore = require('connect-mongo')
+const MongoStore = require("connect-mongo");
 
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-module.exports = app => {
-  app.set('trust proxy', 1);
+module.exports = (app) => {
+  app.set("trust proxy", 1);
   app.use(
     session({
       secret: process.env.SESS_SECRET,
       resave: true,
       saveUninitialized: false,
       cookie: {
-        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+        secure: process.env.NODE_ENV === "production",
         httpOnly: true,
-        maxAge: 6000 * 60
+        maxAge: 6000 * 60,
       },
       store: MongoStore.create({
-        mongoUrl: process.env.MONGODB_URL || "mongodb://localhost/find-on-streaming-app"
-      })
+        mongoUrl:
+          process.env.MONGODB_URI ||
+          "mongodb://localhost/find-on-streaming-app",
+      }),
     })
   );
 };
