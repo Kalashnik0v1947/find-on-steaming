772ff29f24de41089062430a1ccf3f7308af356e
Sergey Kadiyevskiy <51814805+Kalashnik0v1947@users.noreply.github.com>
2022-02-17 15:12:27 -0500

    updated

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("signup");
+  res.render("auth/signup");
 });
 
 module.exports = router;
